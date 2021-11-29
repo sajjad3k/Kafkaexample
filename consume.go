61413b5117ee08a45586af3ad13a5f39628a4170
Sajjad <92301113+sajjad3k@users.noreply.github.com>
2021-11-29 14:13:30 +0530

    Added SASL Authentication

--- consume.go
+++ consume.go
@@ -5,14 +5,29 @@ import (
 	"encoding/json"
 
 	"github.com/segmentio/kafka-go"
+	"github.com/segmentio/kafka-go/sasl/plain"
 )
 
 func Consume(ctx context.Context) []Stock {
 
 	var Stockslist []Stock
+	
+	mech := plain.Mechanism{
+		Username: username,
+		Password: password,
+	}
+
+	dial := kafka.Dialer{
+		Timeout:       10 * time.Second,
+		DualStack:     true,
+		SASLMechanism: mech,
+	}
+	
+	
 	read := kafka.NewReader(kafka.ReaderConfig{
 		Brokers: []string{brokerAddress},
 		Topic:   topic,
+		Dialer:  &dial,
 	})
 
 	for {
